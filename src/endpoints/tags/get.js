570920bd065be8fdc840a03314a5ff6a5a841fe0
Mike Hogarth <mikehogarth20@hotmail.com>
2020-01-14 20:20:22 +0000

    format response

--- src/endpoints/tags/get.js
+++ src/endpoints/tags/get.js
@@ -4,8 +4,21 @@ const { success, failure } = require("../response");
 module.exports.handler = async event => {
   try {
     const result = await getRecipesByTag(event.pathParameters.tagId);
-    return success(result.Items);
+    return success(result.Items.map(taggingResponse));
   } catch (err) {
     return failure("UNKNOWN ERROR");
   }
 };
+
+function taggingResponse(item) {
+  // TODO: this is all probably reusable - refactor
+  const { pk, sk } = item;
+  const recipeSlug = pk.slice(pk.indexOf("#") + 1);
+  const taggingSlug = sk.slice(sk.indexOf("#") + 1);
+
+  return {
+    ...item,
+    taggingSlug,
+    recipeSlug
+  };
+}
