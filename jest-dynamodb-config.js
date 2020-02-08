26ee0f6df29a64adc8944c614637b80da3a77f21
Mike Hogarth <mikehogarth20@hotmail.com>
2020-02-08 14:17:43 +0000

    Doh!

--- jest-dynamodb-config.js
+++ jest-dynamodb-config.js
@@ -9,6 +9,14 @@ module.exports = async () => {
     .filter(r => r.Type === "AWS::DynamoDB::Table")
     .map(r => r.Properties);
 
+  // Add StreamEnabled for tests (causes error if added for real deploys)
+  // See https://gitter.im/serverless/serverless?at=59a993009acddb2407f19f40
+  tables.forEach(table => {
+    if (table.StreamSpecification) {
+      table.StreamSpecification.StreamEnabled = true;
+    }
+  });
+
   return {
     tables,
     port: 8000
